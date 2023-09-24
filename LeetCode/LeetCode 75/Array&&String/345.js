2784f918179f529d47ba512733fe56ce2f657001
Roberto Peregrina <111211515+RobertoPeregrinaJr96@users.noreply.github.com>
2023-09-24 15:30:41 -0700

    update

--- LeetCode/LeetCode 75/Array&&String/345.js
+++ LeetCode/LeetCode 75/Array&&String/345.js
@@ -16,7 +16,24 @@ The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower an
 
  */
 var reverseVowels = function (s) {
-
+   let start = 0;
+   let end = s.length - 1;
+   const VOWELS = new Set(["a", "i", "u", "e", "o","A", "I", "U", "E", "O"])
+   const ans = [...s];
+   while(start < end){
+     if(!VOWELS.has(ans[start]))
+       start++;
+     if(!VOWELS.has(ans[end]))
+       end--;
+     if(VOWELS.has(ans[start]) && VOWELS.has(ans[end])){
+       const temp = ans[start];
+       ans[start] = ans[end];
+       ans[end] = temp;
+       start++
+       end--;
+     }
+   }
+   return ans.join("");
 };
 
 /*
